090749629597287d1c6976dea8296427316c2b72
yuanlixiao <yuanlixiao@live.cn>
2014-08-30 01:34:46 +0800

    Init
    Add Proj

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,124 @@
+#include "HelloWorldScene.h"
+
+USING_NS_CC;
+
+CCScene* HelloWorld::scene()
+{
+    // 'scene' is an autorelease object
+    CCScene *scene = CCScene::create();
+    
+    // 'layer' is an autorelease object
+    HelloWorld *layer = HelloWorld::create();
+
+    // add layer as a child to scene
+    scene->addChild(layer);
+
+    // return the scene
+    return scene;
+}
+
+// on "init" you need to initialize your instance
+bool HelloWorld::init()
+{
+    //////////////////////////////
+    // 1. super init first
+    if ( !CCLayer::init() )
+    {
+        return false;
+    }
+    
+    CCSize visibleSize = CCDirector::sharedDirector()->getVisibleSize();
+    CCPoint origin = CCDirector::sharedDirector()->getVisibleOrigin();
+
+    /////////////////////////////
+    // 2. add a menu item with "X" image, which is clicked to quit the program
+    //    you may modify it.
+
+    // add a "close" icon to exit the progress. it's an autorelease object
+    CCMenuItemImage *pCloseItem = CCMenuItemImage::create(
+                                        "CloseNormal.png",
+                                        "CloseSelected.png",
+                                        this,
+                                        menu_selector(HelloWorld::menuCloseCallback));
+    
+	pCloseItem->setPosition(ccp(origin.x + visibleSize.width - pCloseItem->getContentSize().width/2 ,
+                                origin.y + pCloseItem->getContentSize().height/2));
+
+    // create menu, it's an autorelease object
+    CCMenu* pMenu = CCMenu::create(pCloseItem, NULL);
+    pMenu->setPosition(CCPointZero);
+    this->addChild(pMenu, 1);
+
+    /////////////////////////////
+    // 3. add your codes below...
+
+    // add a label shows "Hello World"
+    // create and initialize a label
+    
+    CCLabelTTF* pLabel = CCLabelTTF::create("Hello World", "Arial", 24);
+    
+    // position the label on the center of the screen
+    pLabel->setPosition(ccp(origin.x + visibleSize.width/2,
+                            origin.y + visibleSize.height - pLabel->getContentSize().height));
+
+    // add the label as a child to this layer
+    this->addChild(pLabel, 1);
+
+    // add "HelloWorld" splash screen"
+    CCSprite* pSprite = CCSprite::create("HelloWorld.png");
+
+    // position the sprite on the center of the screen
+    pSprite->setPosition(ccp(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+
+    // add the sprite as a child to this layer
+    this->addChild(pSprite, 0);
+
+	CCSprite* sp = CCSprite::create("circle.png");
+	sp->setPosition(ccp(100,100));
+	this->addChild(sp);
+
+	sp->setRotation(90);
+
+	CCMoveBy* mo1=CCMoveBy::create(0.5f,ccp(100,0));
+	CCRotateBy* ro1=CCRotateBy::create(0.3f,180);
+	CCMoveBy* mo2=CCMoveBy::create(0.5f,ccp(-100,0));
+	CCRotateBy* ro2=CCRotateBy::create(0.3f,180);
+	CCSequence* seq=CCSequence::create(mo1,ro1,mo2,ro2,NULL);
+	CCRepeatForever* rep=CCRepeatForever::create(seq);
+
+	sp->runAction(rep);
+    
+	setTouchEnabled(true);
+    return true;
+}
+
+
+void HelloWorld::menuCloseCallback(CCObject* pSender)
+{
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_WINRT) || (CC_TARGET_PLATFORM == CC_PLATFORM_WP8)
+	CCMessageBox("You pressed the close button. Windows Store Apps do not implement a close button.","Alert");
+#else
+    CCDirector::sharedDirector()->end();
+#if (CC_TARGET_PLATFORM == CC_PLATFORM_IOS)
+    exit(0);
+#endif
+#endif
+}
+
+bool HelloWorld::ccTouchBegan(CCTouch *pTouch, CCEvent *pEvent)
+{
+	CCLOG("LOL");
+	return true;
+}
+
+void HelloWorld::onEnter()
+{
+	CCDirector::sharedDirector()->getTouchDispatcher()->addTargetedDelegate(this,0,false);  
+	CCLayer::onEnter();  
+}
+
+void HelloWorld::onExit()
+{
+	CCDirector::sharedDirector()->getTouchDispatcher()->removeDelegate(this);  
+	CCLayer::onExit();  
+}
---
+++ Classes/HelloWorldScene.h
@@ -0,0 +1,29 @@
+#ifndef __HELLOWORLD_SCENE_H__
+#define __HELLOWORLD_SCENE_H__
+
+#include "cocos2d.h"
+
+class HelloWorld : public cocos2d::CCLayer
+{
+public:
+    // Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+    virtual bool init();  
+
+    // there's no 'id' in cpp, so we recommend returning the class instance pointer
+    static cocos2d::CCScene* scene();
+    
+    // a selector callback
+    void menuCloseCallback(CCObject* pSender);
+    
+    // implement the "static node()" method manually
+    CREATE_FUNC(HelloWorld);
+	
+    virtual bool ccTouchBegan(cocos2d::CCTouch *pTouch, cocos2d::CCEvent *pEvent);
+    virtual void ccTouchMoved(cocos2d::CCTouch *pTouch, cocos2d::CCEvent *pEvent){};
+    virtual void ccTouchEnded(cocos2d::CCTouch *pTouch, cocos2d::CCEvent *pEvent){};
+    virtual void ccTouchCancelled(cocos2d::CCTouch *pTouch, cocos2d::CCEvent *pEvent){};
+	void onEnter();
+	void onExit();
+};
+
+#endif // __HELLOWORLD_SCENE_H__
